dd428da3e039467e182fc61aaf4cf08081594a43
iuliia bobrova <106148151+iuliiabobrova@users.noreply.github.com>
2022-06-21 18:18:23 +0300

    Update static_text.py

--- tgbot/handlers/strategy_info/static_text.py
+++ tgbot/handlers/strategy_info/static_text.py
@@ -7,5 +7,5 @@ rsi_strategy_link = df_texts.str_info[1]
 strategy_info = df_texts.strategy_choice[3]
 strategy_with_links = (f'{strategy_info}\n'
                        f'\n'
-                       f'<a href="{sma_strategy_link}"><b>Среднее скользящее (cross-SMA)</b></a>\n'
-                       f'<a href="{rsi_strategy_link}"><b>Перепроданность по RSI</b></a>')
+                       f'<a href="{sma_strategy_link}"><b>Перепроданность по RSI</b></a>\n'
+                       f'<a href="{rsi_strategy_link}"><b>Среднее скользящее (cross-SMA)</b></a>')
