3172e56cb5cd7c335a5306058176763153eec0f7
peter <peter1558@yandex.ru>
2022-07-07 07:35:19 +0300

    fix filter short_enabled_flag

--- corestrategy/strategy_rsi.py
+++ corestrategy/strategy_rsi.py
@@ -42,7 +42,7 @@ def calc_actual_signals_rsi(df_shares: DataFrame,
                 df_last_signal = df_hist_sgnls[df_hist_sgnls.figi == figi].tail(1)
                 if not df_last_signal.empty:
                     if (df_last_signal.sell_flag.all() != 1 and df_last_signal.datetime.all() != _now() and
-                            df_shares.loc[df_shares.figi == figi].short_enabled_flag[0]):
+                            df_shares.loc[df_shares.index == figi].short_enabled_flag[0]):
                         date = rsi.index[-1]
                         df_hist_sgnls = save_signal_to_df(buy_flag=0,
                                                           sell_flag=1,
@@ -55,7 +55,7 @@ def calc_actual_signals_rsi(df_shares: DataFrame,
                                                           df_shares=df_shares,
                                                           df=df_hist_sgnls)
                         send_signal_to_strategy_subscribers(df=df_hist_sgnls)
-                elif df_shares.loc[df_shares.figi == figi].short_enabled_flag[0]:
+                elif df_shares.loc[df_shares.index == figi].short_enabled_flag[0]:
                     date = rsi.index[-1]
                     df_hist_sgnls = save_signal_to_df(buy_flag=0,
                                                       sell_flag=1,
