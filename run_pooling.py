a904448573c478520231b96aae322031bfd71b78
i.o.novikov <i.o.novikov@tinkoff.ru>
2022-08-26 23:21:08 +0300

    disable any signals

--- run_pooling.py
+++ run_pooling.py
@@ -10,5 +10,5 @@ from corestrategy.run_strat import run_strategies
 
 if __name__ == "__main__":
     # TODO заменить Thread на Task
-    thr1 = Thread(target=run_strategies).start()
+    # thr1 = Thread(target=run_strategies).start() # Disable for ABC test
     run_pooling()
