c8ea53ba727a58e5f58b13d0b9da52aa361823fc
peter <peter1558@yandex.ru>
2022-08-30 22:50:40 +0300

    update delivery_boy, add 429 except, set sleep between msg to 0.02 instead of 0.04

--- corestrategy/delivery_boy.py
+++ corestrategy/delivery_boy.py
@@ -3,6 +3,7 @@ import asyncio
 from pandas import DataFrame
 from telegram import ParseMode, Bot, error
 
+from corestrategy.utils import _now
 from dtb.settings import TELEGRAM_TOKEN
 
 from tgbot.handlers.strategies.utils import get_last_signals
@@ -12,7 +13,7 @@ from tgbot.models import User
 
 async def send_signal_to_strategy_subscribers(df: DataFrame) -> None:
 
-    signal = get_last_signals(df=df, amount=1)[0]
+    signal = get_last_signals(df=df, amount=1)[0]  # TODO взять из БД
     strategy_id = signal.strategy_id
 
     users_with_strategy = await User.get_users_with_strategy_subscription(strategy_id=strategy_id)
@@ -28,11 +29,20 @@ async def send_signal_to_strategy_subscribers(df: DataFrame) -> None:
                 chat_id=user_id,
                 disable_web_page_preview=True,
                 parse_mode=ParseMode.HTML,
-                reply_markup=make_keyboard_for_signal(user_id=user_id, signal=signal)
+                reply_markup=make_keyboard_for_signal(user_id=user_id, signal=signal),
+                timeout=3
             )
-            await asyncio.sleep(0.04)
+            await asyncio.sleep(0.02)
         except error.Unauthorized:
-            success = False
+            with Exception as e:
+                print(f'error when sending signal to user: {e}         {_now()}')
+                print(f'user {user_id} is blocked, error refused')
+            User.objects.filter(user_id=user_id).update(is_blocked_bot=True)
+        except error.RetryAfter(10):
+            print(f'ATTENTION! Too fast message sending')
+            print(f'sleeping 10 seconds')
+            continue
         else:
-            success = True
-
+            with Exception as e:
+                print(f'error when sending signal to user: {e}         {_now()}')
+            continue
